73739a3c0a58d0d69b75fb347d2be69253bca8ce
Suraj32 <suraj21071998@gmail.com>
2019-02-08 15:53:28 +0530

    all erb converted to haml

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,19 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'users#index'
+
+  resources :canteens do
+  	resources :attendance_details
+  	resources :memberships do
+  		resources :payments
+  	end
+  end
+
+  resources :customers do
+  	resources :attendance_details
+  	resources :memberships do
+  		resources :payments
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
