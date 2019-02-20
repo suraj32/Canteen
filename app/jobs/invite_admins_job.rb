5d5142630379be4e1392cc00750c8a35cc782c78
Suraj32 <suraj21071998@gmail.com>
2019-02-20 14:11:30 +0530

    Test cases handled for inserting canteen admin through csv

--- app/jobs/invite_admins_job.rb
+++ app/jobs/invite_admins_job.rb
@@ -4,22 +4,32 @@ class InviteAdminsJob < ApplicationJob
 
   def perform(csv_file_id)
     csv_file = CsvFile.find(csv_file_id)
-  	csv = CSV.parse(csv_file.data, :headers => true)
+    csv = CSV.parse(csv_file.data, :headers => true)
     new_csv = CSV.generate do |new_csv|
-      new_csv << ['Email', 'Canteen name', 'Inserted or not']
-    	csv.each do |row|
-        # add in hand data i.e email n canteen name
-        # then processing 
-        # insert only processing starus
+      new_csv << ['Email', 'Canteen name', 'Status']
+      csv.each do |row|
         canteen = Canteen.find_by(name: row["Canteen name"])
-    		if canteen && row["Email"]
-          User.invite!(email: row["Email"], canteen_id: canteen.id)
-          new_csv << [row["Email"], row["Canteen name"], 'Yes']
-    		else
-    			new_csv << [row["Email"], row["Canteen name"], 'No']
-    		end
-    	end
+        if canteen
+          if row["Email"]
+            user = User.find_by(email: row['Email'])
+            if user && !user.invitation_token
+              new_csv << [row["Email"], row["Canteen name"], 'Admin with email already exist']
+            else
+              user = User.invite!(email: row["Email"], canteen_id: canteen.id)
+              if user.errors.messages == {}
+                new_csv << [row["Email"], row["Canteen name"], 'Admin successfully invited']
+              else
+                new_csv << [row["Email"], row["Canteen name"], "Email #{user1.errors.messages[:email].join()}"]
+              end
+            end
+          else
+            new_csv << [row["Email"], row["Canteen name"], 'Email is blank']
+          end
+        else
+          new_csv << [row["Email"], row["Canteen name"], 'Invalid canteen name']
+        end
+      end
     end
-  	UserMailer.with(content: new_csv).send_csv_report.deliver_later
+    UserMailer.with(content: new_csv).send_csv_report.deliver_now
   end
 end
